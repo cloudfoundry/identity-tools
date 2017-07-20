a8011382e922598b772cacd90bc8653287e6ac2e
Filip Hanik <fhanik@pivotal.io>
2017-07-20 13:03:47 -0700

    Upgrade to Spring Boot 1.5.4
    [#149181375] https://www.pivotaltracker.com/story/show/149181375

--- statsd/src/main/java/org/cloudfoundry/identity/statsd/Application.java
+++ statsd/src/main/java/org/cloudfoundry/identity/statsd/Application.java
@@ -1,19 +1,6 @@
-package org.cloudfoundry.identity.statsd;
-
-import com.timgroup.statsd.NonBlockingStatsDClient;
-import com.timgroup.statsd.StatsDClient;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.lang.management.ManagementFactory;
-
 /*******************************************************************************
  * Cloud Foundry
- * Copyright (c) [2009-2015] Pivotal Software, Inc. All Rights Reserved.
+ * Copyright (c) [2009-2017] Pivotal Software, Inc. All Rights Reserved.
  * <p/>
  * This product is licensed to you under the Apache License, Version 2.0 (the "License").
  * You may not use this product except in compliance with the License.
@@ -23,6 +10,19 @@ import java.lang.management.ManagementFactory;
  * subcomponents is subject to the terms and conditions of the
  * subcomponent's license, as noted in the LICENSE file.
  *******************************************************************************/
+
+package org.cloudfoundry.identity.statsd;
+
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
 @EnableScheduling
